d304101d0fbb71b5ab3d51c47b444c3cc465f782
pablojg9 <pablojuniorgn2@gmail.com>
2021-09-09 09:00:23 -0300

    Layers

--- backend/src/main/java/com/pablo/dsvendas/controller/SellerController.java
+++ backend/src/main/java/com/pablo/dsvendas/controller/SellerController.java
@@ -1,9 +1,14 @@
 package com.pablo.dsvendas.controller;
 
+import com.pablo.dsvendas.dto.SellerDTO;
 import com.pablo.dsvendas.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sellers")
@@ -12,6 +17,9 @@ public class SellerController {
     @Autowired
     private SellerService service;
 
-
-
+    @GetMapping
+    public ResponseEntity<List<SellerDTO>> findAll() {
+        List<SellerDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
 }
